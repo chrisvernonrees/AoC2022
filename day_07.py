71df6d3afc481f268eaae0ff913c6926ae80d6ce
chrisvernonrees <119589849+chrisvernonrees@users.noreply.github.com>
2022-12-07 15:07:13 +0000

    Add files via upload

--- day_07.py
+++ day_07.py
@@ -1,22 +1,38 @@
+import copy
+import time
+
+st = time.time()
+
+
+
+direct_dict = {}
+count = 1
+
+with open("2022day7input.txt","r") as f:
+    for line in f:
+            if ((len(line) == len(line.replace('$ cd',''))) == False) and (line.rstrip() != '$ cd ..'):
+                dir = (line.replace('$ cd ','')).rstrip()
+                direct_dict[count] = 0
+                count += 1
+
 def dict_update(dict,open_vec,value):
     for i in range(len(open_vec)):
         dict[open_vec[i]] = dict[open_vec[i]] + value
     return(dict)
 
-direct_dict = {}
 open_vec = []
 count = 1
 
 with open("2022day7input.txt","r") as f:
     for line in f:
-        if '$ cd ' in line:
-            if '..' not in line:
-                direct_dict[count] = 0
+        if (len(line) == len(line.replace('$ cd',''))) == False:
+            dir = (line.replace('$ cd ','')).rstrip()
+            if (dir != '..'):
                 open_vec.append(count)
                 count += 1
             else:
                 open_vec = open_vec[:-1]
-        elif ('$ ls' in line) or ('dir ' in line):
+        elif ((len(line) == len(line.replace('$ ls',''))) == False) or ((len(line) == len(line.replace('dir ',''))) == False):
             pass
         else:
             size, name = (line.rstrip()).split()
@@ -29,7 +45,9 @@ for key in direct_dict:
 print(count)
 
 filtered_dict = {}
-disk_space, free_space = 70000000, 30000000
+
+disk_space = 70000000
+free_space = 30000000
 space_needed = free_space - (disk_space - direct_dict[1])
 
 for key in direct_dict:
@@ -38,3 +56,6 @@ for key in direct_dict:
 
 min_dir = min(filtered_dict, key=filtered_dict.get)
 print(min_dir, filtered_dict[min_dir])
+
+et = time.time()
+print('Time elapsed:', et-st)
